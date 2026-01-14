9cefe9638a6f4281dd8cb01ee22ce70ca866670c
Akhilesh Negi <149097551+AKHILESH2208@users.noreply.github.com>
2026-01-14 13:58:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ app.use(express.json())
 
 // CORS configuration - Allow all origins for production deployment
 app.use(cors({
-  origin: "https://forever-ecom-frontend.netlify.app",
+  origin: [
+      "http://localhost:5173",
+      "https://forever-ecom-frontend.netlify.app",
+    ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "token"],
